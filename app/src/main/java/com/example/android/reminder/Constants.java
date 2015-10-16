1a70fcfff0d6102aae20b0064f76fa623b81220c
Oleg Yakovlev <oleg.yakovlev.06@gmail.com>
2015-10-16 12:42:20 +0300

    added tabs with test fragment

---
+++ app/src/main/java/com/example/android/reminder/Constants.java
@@ -0,0 +1,7 @@
+package com.example.android.reminder;
+
+public class Constants {
+    public static final int TAB_NOTIFICATION = 0;
+    public static final int TAB_2 = 1;
+    public static final int TAB_3 = 2;
+}
